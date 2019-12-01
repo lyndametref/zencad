7be41d0a7e1b259992c00b5adb6262c3dd291813
mirmik <mirmikns@yandex.ru>
2019-12-01 03:48:37 +0300

    'a'

--- zencad/libs/physics.py
+++ zencad/libs/physics.py
@@ -1,4 +1,39 @@
 import zencad.transform
+from zencad.libs.screw import screw
 
-#def evaluate_unit_force(self):
-#	for 
+class physunit(zencad.assemble.unit):
+	def __init__(self, pose, )
+
+
+#def attach_force_model(unit):
+#	unit.force_sources = []
+#	self.output_force = screw()
+#	for ch in self.childs:
+#		unit.force_sources.append(ch)
+
+
+class dof_agent:
+	def __init__(self, unit, parent=None):
+		self.parent = parent
+		self.unit = unit
+
+	def dof_divide(self):
+		raise NotImplementedError
+
+class free_dof_agent(dof_agent):
+	def dof_divide(self, force):
+		self.dalamber = force
+		self.reaction = screw()
+
+class world:
+	def __init__(self):
+		self.free_units = []
+
+	def add_free_unit(self, unit):
+		self.free_units.append(unit)
+
+	def integrate(self):
+		pass
+
+	def onestep(self, delta):
+		print("onestep")
