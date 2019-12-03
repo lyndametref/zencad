d093a37a356b26549432e72dabbf14d60a996041
mirmik <mirmikns@yandex.ru>
2019-12-04 01:00:07 +0300

    'a'

--- zencad/libs/treedy.py
+++ zencad/libs/treedy.py
@@ -17,10 +17,13 @@ class tree_dynamic_solver:
 		for i in range(len(self.inertial_objects)): self.inertial_objects[i].dynno = i
 		for i in range(len(self.kinematic_frames)): self.kinematic_frames[i].dynno = i
 
+		self.find_post_inertia_objects()
+		self.find_pre_kinematic_frames()
+
 	def find_post_inertia_objects(self):
 		for f in self.kinematic_frames:
 			f.post_inertia_objects = []
-			 
+
 			def iteration(unit):
 				if hasattr(unit, "inertial_object"):
 					f.post_inertia_objects.append(unit.inertial_object)
